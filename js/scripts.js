c9ec36f74abd8ac4ec50eb5becc132e82a8bfa35
Taha Berk Amca <amcatahaberk@gmail.com>
2024-12-17 19:36:58 +0300

    a

--- js/scripts.js
+++ js/scripts.js
@@ -45,4 +45,50 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
+    document.querySelectorAll('.read-more-btn').forEach(button => {
+        button.addEventListener('click', function () {
+            const shortText = this.previousElementSibling.querySelector('.short-text');
+            const fullText = this.previousElementSibling.querySelector('.full-text');
+    
+            if (fullText.classList.contains('d-none')) {
+                fullText.classList.remove('d-none');
+                shortText.classList.add('d-none');
+                this.textContent = 'Read Less';
+            } else {
+                fullText.classList.add('d-none');
+                shortText.classList.remove('d-none');
+                this.textContent = 'Read More';
+            }
+        });
+    });
+
+
 });
+
+
+const modals = [
+    { mainImageId: 'main-modal-image-1', thumbnailsId: 'modal-thumbnails-1' },
+    { mainImageId: 'main-modal-image-2', thumbnailsId: 'modal-thumbnails-2' },
+    { mainImageId: 'main-modal-image-3', thumbnailsId: 'modal-thumbnails-3' },
+    { mainImageId: 'main-modal-image-4', thumbnailsId: 'modal-thumbnails-4' },
+    { mainImageId: 'main-modal-image-5', thumbnailsId: 'modal-thumbnails-5' },
+    { mainImageId: 'main-modal-image-6', thumbnailsId: 'modal-thumbnails-6' }
+];
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+
+    modals.forEach(modal => {
+        const mainImage = document.getElementById(modal.mainImageId);
+        const thumbnails = document.querySelectorAll(`#${modal.thumbnailsId} img`);
+
+        if (mainImage && thumbnails.length > 0) {
+            thumbnails.forEach(thumbnail => {
+                thumbnail.addEventListener('click', () => {
+                    mainImage.src = thumbnail.src; 
+                });
+            });
+        }
+    });
+});
+
